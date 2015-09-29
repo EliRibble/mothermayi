717fcc3b3e68823cd28552e018a15b381233f3b0
Eli Ribble <eli@authentise.com>
2015-09-29 11:20:04 -0600

    Raise an error if we can't find a git repo
    Because it's hard to install a hook in a repository we can't find

--- mothermayi/hook.py
+++ mothermayi/hook.py
@@ -3,6 +3,9 @@ import os
 
 LOGGER = logging.getLogger(__name__)
 
+class NoRepoFoundError(Exception):
+    pass
+
 def find_git_repo():
     location = os.path.abspath('.')
     while location != '/':
@@ -10,6 +13,7 @@ def find_git_repo():
         if os.path.exists(check) and os.path.isdir(check):
             return check
         location = os.path.dirname(location)
+    raise NoRepoFoundError("Could not find a git repository (.git) in {}".format(os.path.abspath('.')))
 
 def install():
     repo = find_git_repo()
--- tests/test_hook.py
+++ tests/test_hook.py
@@ -1,4 +1,11 @@
 import mothermayi.hook
+import os
+import pytest
 
 def test_find_git_repo():
     assert mothermayi.hook.find_git_repo()
+
+def test_find_git_repo_non_existent():
+    os.chdir('/tmp')
+    with pytest.raises(mothermayi.hook.NoRepoFoundError):
+        mothermayi.hook.find_git_repo()
