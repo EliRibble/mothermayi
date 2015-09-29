61ef4f3f364e5c8f4df99d8b67f8c3514d1930f7
Eli Ribble <eli@authentise.com>
2015-09-29 14:32:33 -0600

    Add git module
    So far it's untested but it's getting closer to being able to reasonably
    stash and unstash current data

---
+++ mothermayi/git.py
@@ -0,0 +1,19 @@
+import contextlib
+import logging
+import subprocess
+
+LOGGER = logging.getLogger(__name__)
+
+def execute(command):
+    LOGGER.debug("Executing %s", ' '.join(command))
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate(input=None)
+    LOGGER.debug("stdout %s", stdout)
+    LOGGER.debug("stderr %s", stderr)
+
+@contextlib.contextmanager
+def stash():
+    execute(['git', 'stash', '-u', '--keep-index'])
+    yield
+    execute(['git', 'reset', '--hard'])
+    execute(['git', 'stash', 'pop', '--quiet', '--index'])
