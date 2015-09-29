549ba24637b85bec639e5aa71eeb8ab084f3a426
Eli Ribble <eli@authentise.com>
2015-09-29 10:53:26 -0600

    Recursively find the git repository

--- mothermayi/hook.py
+++ mothermayi/hook.py
@@ -1,2 +1,16 @@
+import logging
+import os
+
+LOGGER = logging.getLogger(__name__)
+
+def find_git_repo():
+    location = os.path.abspath('.')
+    while location != '/':
+        check = os.path.join(location, '.git')
+        if os.path.exists(check) and os.path.isdir(check):
+            return check
+        location = os.path.dirname(location)
+
 def install():
-    print('installin')
+    repo = find_git_repo()
+    LOGGER.debug("Found git repo at %s", repo)
