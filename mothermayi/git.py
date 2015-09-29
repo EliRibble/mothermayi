586ae13db6d63b85a873e92bba61cbcd37c50be5
Eli Ribble <eli@authentise.com>
2015-09-29 14:58:50 -0600

    If we have a non-zero exit code then blow up
    This keeps us from doing bad things in a chain of commands like stash if
    any part fails

--- mothermayi/git.py
+++ mothermayi/git.py
@@ -8,6 +8,8 @@ def execute(command):
     LOGGER.debug("Executing %s", ' '.join(command))
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate(input=None)
+    if proc.returncode != 0:
+        raise Exception("Failed to execute command {}".format(' '.join(command)))
     LOGGER.debug("stdout %s", stdout)
     LOGGER.debug("stderr %s", stderr)
 
