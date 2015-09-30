ad316085497fc0c760d8747d3ab2fa07af587959
Eli Ribble <eli@authentise.com>
2015-09-30 13:50:38 -0600

    Add a way to signal that the hook has failed
    We do this by raising FailHook

---
+++ mothermayi/errors.py
@@ -0,0 +1,3 @@
+class FailHook(Exception):
+    pass
+
--- mothermayi/pre_commit.py
+++ mothermayi/pre_commit.py
@@ -1,9 +1,23 @@
+import logging
 import mothermayi.entryway
+import mothermayi.errors
 import mothermayi.git
+import mothermayi.colors
 
-def handle_plugins(config, entries, staged):
-    for entry in entries:
-        result = entry(config, staged)
+LOGGER = logging.getLogger(__name__)
+
+def handle_plugins(config, plugins, staged):
+    for name, plugin in plugins.items():
+        try:
+            result = plugin['pre-commit'](config, staged)
+            status = mothermayi.colors.green("PASSED")
+        except mothermayi.errors.FailHook as e:
+            result = str(e)
+            status = mothermayi.colors.red("FAILED")
+        message = "{0:<10}...[{1:<7}]".format(name, status)
+        print(message)
+        if result:
+            print(result)
 
 def run(config):
     with mothermayi.git.stash():
