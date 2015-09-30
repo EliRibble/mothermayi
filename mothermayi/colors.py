b5d5f377cf3d3b2b4459c36ba47e2e0f4a3125f4
Eli Ribble <eli@authentise.com>
2015-09-30 13:51:00 -0600

    Add module for putting color in our text output

---
+++ mothermayi/colors.py
@@ -0,0 +1,13 @@
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+def green(text):
+    return GREEN + text + ENDC
+
+def red(text):
+    return RED + text + ENDC
